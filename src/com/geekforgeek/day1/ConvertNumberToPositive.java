316be5384fbb54d563556b4e1fdd60f0587f62ae
sairam566 <sairam1431995@gmail.com>
2022-10-08 18:36:04 +0530

    Initial commit: 1st Problem MakeAnumberPositive

---
+++ src/com/geekforgeek/day1/ConvertNumberToPositive.java
@@ -0,0 +1,13 @@
+package com.geekforgeek.day1;
+
+public class ConvertNumberToPositive {
+
+	public static void main(String[] args) {
+		int x=10;
+		if(x<0) {
+			x = -1 * x;
+		}
+		System.out.println(x);
+	}
+
+}
