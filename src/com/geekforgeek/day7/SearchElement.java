fa2c86bf3af5fd35d63c3107c6e11de4e1476b0a
sairam566 <sairam1431995@gmail.com>
2022-10-26 09:59:31 +0530

    Day 7 Search Element index in array

---
+++ src/com/geekforgeek/day7/SearchElement.java
@@ -0,0 +1,21 @@
+package com.geekforgeek.day7;
+
+public class SearchElement {
+
+	public static void main(String[] args) {
+		int[] arr = { 20, 5, 7, 25 };
+		int elementToSearch = 5;
+		int index = searchGivenIndex(arr, elementToSearch, elementToSearch);
+		System.out.println("The index of element " + elementToSearch + " is: " + index);
+	}
+
+	static int searchGivenIndex(int[] arr, int arrLength, int elementToSearch) {
+		for (int i = 0; i < arrLength; i++) {
+			if (arr[i] == elementToSearch) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
